24b23418626829d4664b60c4aeac0fbd256daeaa
Techfo <42501497+AB290817008@users.noreply.github.com>
2023-08-03 16:06:14 +0500

    one

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  Button,
+} from "react-native";
+import { API, Amplify, graphqlOperation } from "aws-amplify";
+import config from "./aws-exports";
+import { addTodo, deleteTodo, updateTodo } from "./src/graphql/mutations";
+import { getTodos } from "./src/graphql/queries";
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+
+Amplify.configure(config);
+
+// const initialState = { id: '', title: '', done: '' };
 
 export default function App() {
+  const [data, setData] = useState();
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(getTodos));
+      // console.log("todoData=>", todoData?.data?.getTodos);
+      const myData = todoData?.data?.getTodos;
+      console.log("myData=>", myData);
+      setData(myData);
+    } catch (e) {
+      console.log("error=>", e);
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  console.log("dataUseState=>", data);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <FlatList
+          keyExtractor={(item) => item.id}
+          data={data}
+          renderItem={({ item }) => <Text>{item.task}</Text>}
+        /> */}
+      {data?.map((item: any) => {
+        return (
+          <View>
+            {" "}
+            <Text>{item.title}</Text>
+            {/* <Button>X</Button> */}
+          </View>
+        );
+      })}
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
